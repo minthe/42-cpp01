a988bf8343cc457f7bfcd05728e75aafb1ee0637
vfuhlenb <vfuhlenb@students.42wolfsburg.de>
2023-02-28 00:05:19 +0100

    ex03 wip

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,29 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Weapon.class.cpp                                   :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/27 14:15:59 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2023/02/27 19:23:53 by vfuhlenb         ###   ########.fr       */
+/*   Created: 2023/02/27 19:24:58 by vfuhlenb          #+#    #+#             */
+/*   Updated: 2023/02/27 19:24:59 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Weapon.class.hpp"
-#include <iostream>
-
-Weapon::Weapon(std::string newType) : _type(newType) {}
-Weapon::~Weapon() {}
-
-std::string	Weapon::getType()
-{
-	std::string&	typeREF = this->_type; // TODO do i need this? or does it generally return a const reference?
-	return (typeREF);
-}
-
-void	Weapon::setType(std::string newType)
-{
-	this->_type = newType;
-	return;
-}
